e7317955a74091582b22ea402bfbbb22c105a078
fujiikei <azyuyu@gmail.com>
2025-01-11 21:39:25 +0900

    button_css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 export const App = () => {
+  const onClickButton = () => {
+    alert('button clicked');
+  };
+  const contentStyle = {
+    color: 'blue',
+    fontSize: '24px',
+  };
   return (
     <div>
-      <h1>Hello World!!!</h1>
+      <h1 id="message">Hello World!!!</h1>
+      <p style={contentStyle}>This is a sample paragraph in blue.</p>
+      <button onClick={onClickButton}>button</button>
     </div>
   );
 };
